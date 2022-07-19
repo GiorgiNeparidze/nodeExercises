15a551512c26a4a29b354dd513c609987b7dbe2a
Giorgi Neparidze <cs.neparidze@gmail.com>
2022-07-19 19:59:07 +0200

    finished Command-Line Art

---
+++ Command-LineArt/giorgiArted.js
@@ -0,0 +1,19 @@
+let figlet = require("figlet");
+
+figlet.text(
+  "Giorgi!",
+  {
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
