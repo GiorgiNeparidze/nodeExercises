f8bf66844f945a0d6c573d122d3307a30d96169f
Giorgi Neparidze <cs.neparidze@gmail.com>
2022-07-20 19:48:07 +0200

    finished A test-driven HTTP response

---
+++ ATest-drivenHTTPResponse/app.js
@@ -0,0 +1,15 @@
+const { createServer } = require("node:http");
+
+function createApp() {
+  return createServer((request, response) => {
+    response.statusCode = 200;
+
+    response.setHeader("Content-Type", "text/html");
+
+    const jsonResponseBody = "<html><body><h1>Stavo Preparando Per Un Eservcizio Pesante... :D</h1></body></html>";
+
+    response.end(jsonResponseBody);
+  });
+}
+
+module.exports = createApp;
--- ATest-drivenHTTPResponse/app.test.js
+++ ATest-drivenHTTPResponse/app.test.js
@@ -10,7 +10,7 @@ test("GET /", async () => {
   const response = await request
     .get("/")
     .expect(200)
-    .expect("Content-Type", "application/json");
+    .expect("Content-Type", "text/html");
 
-  expect(response.body).toEqual({ location: "Earth" });
+  expect(response.body).toBe("<html><body><h1>Stavo Preparando Per Un Eservcizio Pesante</h1></body></html>");
 });
