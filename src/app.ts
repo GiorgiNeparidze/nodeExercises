3467b395c9e3425a680588c917d1b940fae3d6aa
Giorgi Neparidze <cs.neparidze@gmail.com>
2022-08-05 01:52:36 +0200

    finished Retrieve, Update And Delete

--- src/app.ts
+++ src/app.ts
@@ -18,23 +18,73 @@ app.get("/all", async (request, response) => {
   response.json(cities);
 });
 
-app.post(
-  "/cities",
+app.get("/:id(\\d+)", async (req, res, next) => {
+  try {
+    const cityId: number = Number(req.params.id);
+
+    const city = await prisma.cities.findUnique({ where: { id: cityId } });
+
+    if (!city) {
+      res.status(404);
+      return next(`Can NOT Get City With ID: ${cityId}`);
+    }
+
+    res.json(city);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+app.post("/cities", validate({ body: citySchema }), async (req, res, next) => {
+  try {
+    const CityData: CityData = req.body;
+    const city = await prisma.cities.create({
+      data: CityData,
+    });
+
+    res.status(201).json(`Correctly added city with ID: ${city.id}`);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
+
+app.patch(
+  "/:id(\\d+)",
   validate({ body: citySchema }),
   async (req, res, next) => {
     try {
       const CityData: CityData = req.body;
-      const city = await prisma.cities.create({
+      const { id } = req.params;
+      const city = await prisma.cities.update({
+        where: { id: Number(id) },
         data: CityData,
       });
 
-      res.status(201).json(`Correctly added city with ID: ${city.id}`);
+      res.status(201).json(`Correctly Updated City With ID: ${city.id}`);
     } catch (error) {
       res.status(400).json(error);
       return next(error);
     }
   }
 );
+
+app.delete("/:id(\\d)", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const city = await prisma.cities.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.status(200).json(`Correctly Deleted City With ID: ${city.id}`);
+  } catch (error) {
+    res.status(400).json(error);
+    return next(error);
+  }
+});
 
 app.use(ValidationErrorMiddleware);
 
