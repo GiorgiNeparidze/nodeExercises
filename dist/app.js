d9ed4fcd9108e1dd5d53cda55cc66214bb5c7094
Giorgi Neparidze <cs.neparidze@gmail.com>
2022-08-05 00:26:02 +0200

    Finished Create A Resource

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const client_1 = require("@prisma/client");
+const validation_1 = require("./validation");
 const app = (0, express_1.default)();
 const prisma = new client_1.PrismaClient();
+app.use(express_1.default.json());
 app.get("/all", async (request, response) => {
     const cities = await prisma.cities.findMany();
     response.json(cities);
 });
+app.post("/cities", (0, validation_1.validate)({ body: validation_1.citySchema }), async (req, res, next) => {
+    try {
+        const CityData = req.body;
+        const city = await prisma.cities.create({
+            data: CityData,
+        });
+        res.status(201).json(`Correctly added city with ID: ${city.id}`);
+    }
+    catch (error) {
+        res.status(400).json(error);
+        return next(error);
+    }
+});
+app.use(validation_1.ValidationErrorMiddleware);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,41 @@
 import express from "express";
 import "express-async-errors";
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+
+import {
+  validate,
+  ValidationErrorMiddleware,
+  citySchema,
+  CityData,
+} from "./validation";
 
 const app = express();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+app.use(express.json());
 
 app.get("/all", async (request, response) => {
-  const cities = await prisma.cities.findMany()
-  response.json(cities)
+  const cities = await prisma.cities.findMany();
+  response.json(cities);
 });
+
+app.post(
+  "/cities",
+  validate({ body: citySchema }),
+  async (req, res, next) => {
+    try {
+      const CityData: CityData = req.body;
+      const city = await prisma.cities.create({
+        data: CityData,
+      });
+
+      res.status(201).json(`Correctly added city with ID: ${city.id}`);
+    } catch (error) {
+      res.status(400).json(error);
+      return next(error);
+    }
+  }
+);
+
+app.use(ValidationErrorMiddleware);
 
 export default app;
